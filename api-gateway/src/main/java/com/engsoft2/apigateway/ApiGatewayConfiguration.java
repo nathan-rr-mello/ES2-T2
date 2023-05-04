7e754498f1557a0c052fd8210f16d47f1642b776
nathan-rr-mello <nathanreismello2@gmail.com>
2023-05-04 00:37:14 -0300

    test with cb on api-gateway

--- api-gateway/src/main/java/com/engsoft2/apigateway/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/engsoft2/apigateway/ApiGatewayConfiguration.java
@@ -1,13 +1,45 @@
 package com.engsoft2.apigateway;
 
+import java.time.Duration;
+
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+
 @Configuration
 public class ApiGatewayConfiguration {
+
+	@Bean
+	public CircuitBreaker circuitBreaker(){
+		CircuitBreakerConfig config = CircuitBreakerConfig.custom()
+										.failureRateThreshold(50)
+										.waitDurationInOpenState(Duration.ofSeconds(5))
+										.build();
+		CircuitBreakerRegistry reg = CircuitBreakerRegistry.of(config);
+		return reg.circuitBreaker("myCircuitBreaker");
+	}
+
     @Bean
+	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p.path("/currency-exchange/**")
+						//.filters(f -> f.circuitBreaker(c -> c.setName("myCircuitBreaker")))
+						.uri("lb://currency-exchange"))
+				.route(p -> p.path("/currency-conversion/**")
+						.uri("lb://currency-conversion"))
+				.route(p -> p.path("/currency-conversion-feign/**")
+						.uri("lb://currency-conversion"))
+				.route(p -> p.path("/circuit-breaker/**") 
+						.uri("lb://circuit-breaker"))
+				.build();
+	}    
+
+   /*@Bean
 	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route(p -> p.path("/currency-exchange/**")
@@ -16,8 +48,8 @@ public class ApiGatewayConfiguration {
 						.uri("lb://currency-conversion"))
 				.route(p -> p.path("/currency-conversion-feign/**")
 						.uri("lb://currency-conversion"))
-				.route(p -> p.path("/circuit-breaker/**") //TEST
+				.route(p -> p.path("/circuit-breaker/**") 
 						.uri("lb://circuit-breaker"))
 				.build();
-	}    
+	}    */
 }
