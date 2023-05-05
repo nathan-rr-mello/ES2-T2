b27eeccb700b11e7fc8ba98ec05d4609438c6330
nathan-rr-mello <nathanreismello2@gmail.com>
2023-05-05 02:14:59 -0300

    gateway config

--- api-gateway/src/main/java/com/engsoft2/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/engsoft2/apigateway/ApiGatewayApplication.java
@@ -2,11 +2,17 @@ package com.engsoft2.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ApiGatewayApplication {
 
-	
+	@RequestMapping("/fallback")
+	public String fallback() {
+		return "This is a fallback";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
--- api-gateway/src/main/java/com/engsoft2/apigateway/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/engsoft2/apigateway/ApiGatewayConfiguration.java
@@ -1,34 +1,28 @@
 package com.engsoft2.apigateway;
 
-import java.time.Duration;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.github.resilience4j.circuitbreaker.CircuitBreaker;
-import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
-import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
-
 @Configuration
 public class ApiGatewayConfiguration {
 
-	@Bean
-	public CircuitBreaker circuitBreaker(){
-		CircuitBreakerConfig config = CircuitBreakerConfig.custom()
-										.failureRateThreshold(50)
-										.waitDurationInOpenState(Duration.ofSeconds(5))
-										.build();
-		CircuitBreakerRegistry reg = CircuitBreakerRegistry.of(config);
-		return reg.circuitBreaker("myCircuitBreaker");
-	}
+	// @Bean
+	// public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+	// 	return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+	// 			.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build())
+	// 			.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+	// 			.build());
+	// }
 
     @Bean
 	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route(p -> p.path("/currency-exchange/**")
-						//.filters(f -> f.circuitBreaker(c -> c.setName("myCircuitBreaker")))
+						.filters(f -> f.circuitBreaker(c -> c.setName("myCircuitBreaker").setFallbackUri("forward:/fallback")))
+						//.rewritePath("/currency-exchange/**", "/backingServiceEndpoint"))
 						.uri("lb://currency-exchange"))
 				.route(p -> p.path("/currency-conversion/**")
 						.uri("lb://currency-conversion"))
@@ -38,18 +32,4 @@ public class ApiGatewayConfiguration {
 						.uri("lb://circuit-breaker"))
 				.build();
 	}    
-
-   /*@Bean
-	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route(p -> p.path("/currency-exchange/**")
-						.uri("lb://currency-exchange"))
-				.route(p -> p.path("/currency-conversion/**")
-						.uri("lb://currency-conversion"))
-				.route(p -> p.path("/currency-conversion-feign/**")
-						.uri("lb://currency-conversion"))
-				.route(p -> p.path("/circuit-breaker/**") 
-						.uri("lb://circuit-breaker"))
-				.build();
-	}    */
 }
