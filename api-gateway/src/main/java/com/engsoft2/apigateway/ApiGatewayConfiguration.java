9ca41ac45cb0cdbdc4408664dff2c9cf8b8113f3
nathan-rr-mello <nathanreismello2@gmail.com>
2023-05-05 21:05:55 -0300

    config via code

--- api-gateway/src/main/java/com/engsoft2/apigateway/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/engsoft2/apigateway/ApiGatewayConfiguration.java
@@ -1,21 +1,29 @@
 package com.engsoft2.apigateway;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+
 @Configuration
 public class ApiGatewayConfiguration {
 
-	// @Bean
-	// public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
-	// 	return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
-	// 			.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build())
-	// 			.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
-	// 			.build());
-	// }
+	@Bean
+	public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+	 	return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+	 			.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build())
+	 			.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+	 			.build());
+	}
 
     @Bean
 	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
